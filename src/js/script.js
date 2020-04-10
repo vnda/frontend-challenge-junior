5291e235869851950e7e91fb9044aa23fccd168b
Erli Junior <erlijunior@outlook.com>
2020-04-09 21:32:28 -0300

    Finalizado layout responsivo

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
   //Slider Home
-  var sliderHome = new Swiper ('.slider-home', {
+  const sliderHome = new Swiper ('.slider-home', {
     loop: true,
     slidesPerView: 1,
     // Navigation arrows
@@ -11,8 +11,37 @@ $(document).ready(function () {
     },
   });
 
+  //Slider Marcas
+  const sliderMarcas = new Swiper ('.slider-marcas', {
+    loop: false,
+    slidesPerView: 1,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 4,
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 5,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 6,
+      }
+    }
+  });
+
   //Slider Produtos
-  var sliderProdutos = new Swiper ('.slider-produtos', {
+  const sliderProdutos = new Swiper ('.slider-produtos', {
     loop: true,
     slidesPerView: 1,
     // Navigation arrows
@@ -23,18 +52,31 @@ $(document).ready(function () {
   });
 
   //Slider Insta
-  var sliderInsta = new Swiper ('.slider-insta', {
+  const sliderInsta = new Swiper ('.slider-insta', {
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 20,
     centeredSlides: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 3,
+      },
+    }
   });
 
   //Mostra busca
-  var btnBusca = document.querySelector('.btn-busca');
-  var formBusca = document.querySelector('.form-busca');
+  const btnBusca = document.querySelector('.btn-busca');
+  const formBusca = document.querySelector('.form-busca');
   btnBusca.addEventListener('click', function (e) {
     formBusca.classList.toggle('show');
   });
+
+  //Abrir menu Mobile
+  const btnMenu = document.querySelector('.hamburger');
+  const menu = document.querySelector('.nav-menu');
+  btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+
 
 });
