6397c24a90fe53c65ccea71ccc1f42fce16e337c
Marcus Gonçalves <marcus.goncalvezz@outlook.com>
2020-07-23 06:26:40 -0300

    slider finished

---
+++ dist/scripts/scripts.js
@@ -0,0 +1,1 @@
+var mySwiper=new Swiper(".swiper-container",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
---
+++ src/js/swiper.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
