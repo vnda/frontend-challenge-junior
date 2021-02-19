cd695a14746500cb37272a1fe44745aa52634a21
Jade Oliveira <jade.ufrgs@gmail.com>
2021-02-19 20:02:37 -0300

    Add project files

---
+++ dist/js/swiper.js
@@ -0,0 +1,1 @@
+const swiper=new Swiper(".swiper-container",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
