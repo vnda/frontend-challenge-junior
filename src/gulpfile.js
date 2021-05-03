ffe2b9da05b224220339cc233032d7522b9f27b1
ismael <ismael.rodino@hotmai.com>
2021-05-02 21:57:35 -0300

    Junior Challenge done

---
+++ src/gulpfile.js
@@ -0,0 +1,50 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask(){
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask(){
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }
+  });
+  cb();
+}
+
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
