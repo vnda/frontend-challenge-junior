524bb63c49f70143f1f5a78cede7d929441a1917
guimaraesgui <guimaraes11santos1@gmail.com>
2021-06-20 23:15:53 -0300

    upando projeto

---
+++ vnda/src/js/swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
