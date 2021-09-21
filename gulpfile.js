24723d6874755bc0b7e3b1e53414d21374d63b6d
natalia-lavarda <natalia.lavarda@gmail.com>
2021-09-21 02:33:53 -0300

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
+
+gulp.task('copy', ['clean'], function() { 
+    
+    return gulp.src('src/**/*')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', ['copy'], function() {
+
+        gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
